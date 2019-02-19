f0423ba1801cf1d888f1b2e941e605ffd2a59734
Juan Laube <juanlaube@icloud.com>
2019-02-19 15:47:23 -0300

    :construction: Show calendars

--- Toggl.Giskard/Adapters/UserCalendarsRecyclerAdapter.cs
+++ Toggl.Giskard/Adapters/UserCalendarsRecyclerAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Android.Runtime;
 using Android.Views;
 using Toggl.Foundation.MvvmCross.ViewModels.Selectable;
 using Toggl.Giskard.ViewHolders;
@@ -7,6 +8,10 @@ namespace Toggl.Giskard.Adapters
 {
     public sealed class UserCalendarsRecyclerAdapter : BaseRecyclerAdapter<SelectableUserCalendarViewModel>
     {
-
+        protected override BaseRecyclerViewHolder<SelectableUserCalendarViewModel> CreateViewHolder(ViewGroup parent, LayoutInflater inflater, int viewType)
+        {
+            var inflatedView = inflater.Inflate(Resource.Layout.UserCalendarItem, parent, false);
+            return new UserCalendarViewHolder(inflatedView);
+        }
     }
 }
--- Toggl.Giskard/Fragments/SelectUserCalendarsFragment.cs
+++ Toggl.Giskard/Fragments/SelectUserCalendarsFragment.cs
@@ -1,13 +1,19 @@
 ﻿using Android.OS;
+using Android.Support.V7.Widget;
 using Android.Views;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
 using Toggl.Foundation.MvvmCross.ViewModels.Calendar;
+using Toggl.Giskard.Adapters;
 using Toggl.Giskard.Extensions.Reactive;
 using Toggl.Multivac.Extensions;
 
 namespace Toggl.Giskard.Fragments
 {
+    [MvxFragmentPresentation(AddToBackStack = true)]
     public sealed partial class SelectUserCalendarsFragment : ReactiveFragment<SelectUserCalendarsViewModel>
     {
+        private UserCalendarsRecyclerAdapter userCalendarsAdapter;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
@@ -27,12 +33,19 @@ namespace Toggl.Giskard.Fragments
                 .BindAction(ViewModel.Done)
                 .DisposedBy(DisposeBag);
 
+            userCalendarsAdapter
+                .ItemTapObservable
+                .Subscribe(ViewModel.SelectCalendar.Inputs)
+                .DisposedBy(DisposeBag);
+
             return view;
         }
 
         private void setupRecyclerView()
         {
-
+            userCalendarsAdapter = new UserCalendarsRecyclerAdapter();
+            recyclerView.SetAdapter(userCalendarsAdapter);
+            recyclerView.SetLayoutManager(new LinearLayoutManager(Context));
         }
     }
 }

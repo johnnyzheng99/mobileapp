29b89cda39fcc28b8bd0a0431f23a30da4504b0d
Juan Laube <juanlaube@icloud.com>
2019-02-19 09:30:19 -0300

    :sparkles: Add empty fragment

---
+++ Toggl.Giskard/Fragments/SelectUserCalendarsFragment.ViewBinds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Android.Views;
+
+namespace Toggl.Giskard.Fragments
+{
+    public sealed partial class SelectUserCalendarsFragment
+    {
+        protected override void InitializeViews(View view)
+        {
+        }
+    }
+}
---
+++ Toggl.Giskard/Fragments/SelectUserCalendarsFragment.cs
@@ -0,0 +1,19 @@
+﻿using Android.OS;
+using Android.Views;
+using Toggl.Foundation.MvvmCross.ViewModels.Calendar;
+
+namespace Toggl.Giskard.Fragments
+{
+    public sealed partial class SelectUserCalendarsFragment : ReactiveFragment<SelectUserCalendarsViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.SelectUserCalendarsFragment, container, false);
+            InitializeViews(view);
+
+            return view;
+        }
+    }
+}
